0bf2fa93d8e9c8bf9c8846eb8b6b76e1c1c26770
MrXerios <82034596+MrXerios@users.noreply.github.com>
2025-05-23 17:19:40 +0200

    Update descriptors.py

--- descriptors.py
+++ descriptors.py
@@ -1,43 +1,31 @@
+from abc import ABC, abstractmethod
+
 import operator
 
+class _ExpressionDescriptor(ABC):
+    """Abstract class for the descriptors defined in this module. This
+    class is designed to be inherited. This class defines the
+    __set_name__ dunder method, as well as any operator overload.
+    """
 
-class Alias:
-    def __init__(self, attribute_path: str):
-        self.names = attribute_path.split(".")
-
-    def __get__(self, owner, objtype=None):
-        if owner is None:
-            return self
-        obj = owner
-        for name in self.names:
-            obj = getattr(obj, name)
-        return obj
-
-    def __set__(self, owner, value):
-        obj = owner
-        for name in self.names[:-1]:
-            obj = getattr(obj, name)
-        setattr(obj, self.names[-1], value)
-
-    def __delete__(self, owner):
-        delattr(obj, self.attribut_name)
-
-
-class OperableProperty:
     def __set_name__(self, owner, name):
         self.name = name
-        self.private_name = "_" + name
-        self.__doc__ = f"OperableProperty '{name}' of {owner!r}."
+        self.__doc__ = (
+            f"{self.__class__.__name__} '{name}' of "
+            f"class '{owner.__name__}'."
+        )
 
+    @abstractmethod
     def __get__(self, owner, ownertype=None):
-        if owner is None:
-            # Necessesary to reference an instance at class creation,
-            # when it does not have an owner yet.
-            return self
-        return getattr(owner, self.private_name)
+        return
 
+    @abstractmethod
     def __set__(self, owner, val):
-        setattr(owner, self.private_name, val)
+        pass
+
+    @abstractmethod
+    def __delete__(self, owner):
+        pass
 
     def __mul__(self, other):
         return Result(operator.mul, self, other)
@@ -63,29 +51,102 @@ class OperableProperty:
     def __rsub__(self, other):
         return Result(operator.sub, other, self)
 
-    # ...More operations could be defined, but I am lazy
+    # More operations could be defined...
+    # ...but I'm lazy and I don't need them.
+
+class OperableProperty(_ExpressionDescriptor):
+    """A descriptor for an attribute of a class on which operations can
+    be done. This attribute can be set or deleted if necessary.
+    """
+    def __set_name__(self, owner, name):
+        super().__set_name__(owner, name)
+        self.private_name = "_" + name
+
+    def __get__(self, owner, ownertype=None):
+        if owner is None:
+            # Necessesary to reference an instance at class creation,
+            # when it does not have an owner yet. This is what allows
+            # the use of expressions.
+            return self
+        try:
+            a = getattr(owner, self.private_name)
+        except AttributeError as e:
+            msg = str(e).replace(self.private_name, self.name)
+            raise AttributeError(msg) from e
+        return a
+
+    def __set__(self, owner, val):
+        setattr(owner, self.private_name, val)
+
+    def __delete__(self, owner):
+        delattr(owner, self.private_name)
 
 
+class Result(_ExpressionDescriptor):
+    """The result of an operation. This result can be used in other
+    expressions.
 
-class Result(OperableProperty):
-    def __init__(self, operation, *values):
+    Args:
+        operation (Callable) : function used to compute the result
+                               at runtime, when requested.
+        values (Any) : values to use as parameters for the operation
+        kwargs : keyword arguments of operation, if applicable.
+    """
+    def __init__(self, operation, *values, **kwargs):
         self.operation = operation
         self.values = values
+        self.kwargs = kwargs
 
     def __get__(self, owner, ownertype=None):
         if owner is None:
             return self
-        values = []
-        for v in self.values:
-            if isinstance(v, OperableProperty):
-                values.append(v.__get__(owner))
-            elif isinstance(v, str) and v.startswith('@'):
-                values.append(getattr(owner, v[1:]))
-            else:
-                values.append(v)
-        return self.operation(*values)
+        values = [
+            v.__get__(owner) if isinstance(v, _ExpressionDescriptor) else v
+            for v in self.values
+        ]
+        return self.operation(*values, **self.kwargs)
 
     def __set__(self, owner, value):
-        raise AttributeError(
-            f"{self.name} is the result of an operation and is not writable"
-        )
+        msg = f"{self.name} is the result of an operation and is not writable"
+        raise AttributeError(msg)
+
+    def __delete__(self, owner):
+        msg = f"{self.name} is the result of an operation and is not deletable"
+        raise AttributeError(msg)
+
+
+class Alias(_ExpressionDescriptor):
+    """"""
+    def __init__(self, attribute_path: str):
+        self.attribute_path = attribute_path
+        self.names_list = attribute_path.split(".")
+        self.attribute_name = self.names_list[-1]
+
+    def __set_name__(self, owner, name):
+        super().__set_name__(owner, name)
+        self.__doc__ = f"Alias of '{owner.__name__}.{self.attribute_path}'"
+
+    def _get_last_object(self, owner):
+        obj = owner
+        for name in self.names_list[:-1]:
+            obj = getattr(obj, name)
+        return obj
+
+    def __get__(self, owner, objtype=None):
+        if owner is None:
+            return self
+        obj = self._get_last_object(owner)
+        try:
+            a = getattr(obj, self.attribute_name)
+        except AttributeError as e:
+            msg = str(e) + " (accessed from alias)"
+            raise AttributeError(msg) from e
+        return a
+
+    def __set__(self, owner, value):
+        obj = self._get_last_object(owner)
+        setattr(obj, self.attribute_name, value)
+
+    def __delete__(self, owner):
+        obj = self._get_last_object(owner)
+        delattr(obj, self.attribute_name)
